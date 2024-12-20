88f9d4502e76a630f5bcf71982c9c95c05b1add8
Tupacao <arthur12268@gmail.com>
2024-12-20 10:49:23 -0300

    Teste commit

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -3,7 +3,7 @@ package com.example;
 import io.micronaut.runtime.Micronaut;
 
 public class Application {
-
+    // Teste commit
     public static void main(String[] args) {
         Micronaut.run(Application.class, args);
     }
